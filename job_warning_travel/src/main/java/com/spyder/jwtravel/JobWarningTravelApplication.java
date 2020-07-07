49f916652ae68614037248672b5473baef6b2e7f
anderson.silva <infoander@gmail.com>
2020-07-07 18:54:35 -0300

    Create struct and pock to execute process batch

---
+++ job_warning_travel/src/main/java/com/spyder/jwtravel/JobWarningTravelApplication.java
@@ -0,0 +1,13 @@
+package com.spyder.jwtravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobWarningTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobWarningTravelApplication.class, args);
+	}
+
+}
---
+++ job_warning_travel/src/test/java/com/spyder/jwtravel/JobWarningTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spyder.jwtravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobWarningTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
